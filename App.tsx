a05f357b556347628ab3cad19a48019441996312
Dev-Ericles <informaticaericles@gmail.com>
2024-09-30 22:15:27 -0300

    Adicionando Text, TextInput e TouchableOpacity

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Digite seu nome</Text>
+      <TextInput placeholder='Nome' style={styles.textinp}/>
+      <TouchableOpacity activeOpacity={0.7} style={styles.button}>
+      <Text>Enviar</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#A9A9A9',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textinp: {
+    backgroundColor: '#4F4F4F',
+    width: 300,
+    height: 50,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#DCDCDC',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+    width: 300,
   },
 });
